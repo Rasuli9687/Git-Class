a9235a8a31790c9e825e747687e62e122fb1f579
Rasuli9687 <fraidoonrasuli01@gmail.com>
2022-09-28 13:56:13 -0400

    third commit

--- src/Class03B14/Class03.java
+++ src/Class03B14/Class03.java
@@ -3,8 +3,9 @@ package Class03B14;
 public class Class03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hey this is git class");
+		System.out.println("hey this is a change in my project");
+		System.out.println("how are u this is a new version");
 	}
 
 }
